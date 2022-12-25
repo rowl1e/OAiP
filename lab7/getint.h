0ced364dfb7fdcc6cda99da4d047eea1a0ee8c73
rowl1e <112783275+rowl1e@users.noreply.github.com>
2022-12-25 23:12:28 +0300

    Delete lab7 directory

--- lab7/getint.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "getint.c"
-
-void error ();
-int getint ();
-int getint_min (int min);
-int getint_minmax (int min, int max);
--- lab7/strg.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "strg.c"
-
-char* get_str ();
-int str_len(char* str);
-int isletter_ (char c);
